03fe594076fd512fe3d37808a0ce06ed9b1c1d3f
Robert Bernhardt <bernhardt@newsarea.de>
2013-08-13 18:21:46 +0200

    add fsuipcUtil tests

--- fsuipc/src/test/java/de/newsarea/homecockpit/fsuipc/util/FSUIPCUtilTest.java
+++ fsuipc/src/test/java/de/newsarea/homecockpit/fsuipc/util/FSUIPCUtilTest.java
@@ -1,5 +1,6 @@
 package de.newsarea.homecockpit.fsuipc.util;
 
+import de.newsarea.homecockpit.fsuipc.domain.OffsetIdent;
 import de.newsarea.homecockpit.fsuipc.domain.OffsetItem;
 import org.testng.annotations.Test;
 
@@ -19,10 +20,33 @@ public class FSUIPCUtilTest {
 
     @Test
     public void shouldConvertToOffsetItem() {
-        OffsetItem oItem = OffsetItem.from("1, 4, 5000");
+        OffsetItem oItem = FSUIPCUtil.toOffsetItem("1, 4, 5000");
         assertEquals(1, oItem.getOffset());
         assertEquals(4, oItem.getSize());
         assertEquals(5000, oItem.getValue().toInt());
+    }
+
+    @Test
+    public void shouldConvertToOffsetItems() throws Exception {
+        OffsetItem[] oItems = FSUIPCUtil.toOffsetItems("[[ 1, 4, 5000 ], [ 2, 4, 5000 ]]");
+        assertEquals(2, oItems.length);
+        assertEquals("0x00000001 (1) : 4 : 5000 (0x00001388)", oItems[0].toString());
+        assertEquals("0x00000002 (2) : 4 : 5000 (0x00001388)", oItems[1].toString());
+    }
+
+    @Test
+    public void shouldConvertToOffsetIdent() {
+        OffsetIdent oIdent = FSUIPCUtil.toOffsetIdent("1, 4");
+        assertEquals(1, oIdent.getOffset());
+        assertEquals(4, oIdent.getSize());
+    }
+
+    @Test
+    public void shouldConvertToOffsetIdents() throws Exception {
+        OffsetIdent[] oIdents = FSUIPCUtil.toOffsetIdents("[[ 1, 4, 5000 ], [ 2, 4, 5000 ]]");
+        assertEquals(2, oIdents.length);
+        assertEquals("0x00000001 (1) : 4", oIdents[0].toString());
+        assertEquals("0x00000002 (2) : 4", oIdents[1].toString());
     }
 
 }
