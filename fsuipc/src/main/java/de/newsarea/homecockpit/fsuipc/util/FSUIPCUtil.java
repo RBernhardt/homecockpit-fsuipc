062ecc9e431fdc8cb34449dfd4b5a19ede35ee1b
Robert Bernhardt <bernhardt@newsarea.de>
2013-08-20 22:23:51 +0200

    adding tests and javadoc

--- fsuipc/src/main/java/de/newsarea/homecockpit/fsuipc/util/FSUIPCUtil.java
+++ fsuipc/src/main/java/de/newsarea/homecockpit/fsuipc/util/FSUIPCUtil.java
@@ -17,7 +17,7 @@ public final class FSUIPCUtil {
      */
 	public static long toFSUIPCLatitude(double latitude) {
         Assert.isTrue(latitude >= -90, "value is valid from 0 to ±90");
-        Assert.isTrue(latitude <= 90, "value is valid from 0 to ±90");
+        Assert.isTrue(latitude <= +90, "value is valid from 0 to ±90");
         // ~
 		return (long)(latitude / (90D / (10001750D * 65536D * 65536D)));
 	}
@@ -37,8 +37,12 @@ public final class FSUIPCUtil {
      * To convert to Degrees:
      * If your compiler supports long long (64-bit) integers then use such a variable to simply copy this 64-bit value into a double floating point variable and multiply by 360.0/(65536.0 * 65536.0 * 65536.0 * 65536.0).
      * Either way, a negative result is West, positive East.
+     * Die geographische Länge ist ein Winkel, der ausgehend vom Nullmeridian (0°) bis 180° in östlicher und 180° in westlicher Richtung gemessen wird.
      */
 	public static long toFSUIPCLongitude(double longitude) {
+        Assert.isTrue(longitude >= -180, "value is valid from 0 to ±180");
+        Assert.isTrue(longitude <= +180, "value is valid from 0 to ±180");
+        //
 		return (long)(longitude / (360.0 / (65536.0 * 65536.0 * 65536.0 * 65536.0)));
 	}
 
@@ -52,40 +56,59 @@ public final class FSUIPCUtil {
 	}
 	
 	/* */
-	
+
+    /***
+     * Altitude, in metres and fractional metres. The units are in the high 32-bit integer (at 0574) and the fractional part is in the low 32-bit integer (at 0570).
+     */
+    public static long toFSUIPCAlititude(double altitude) {
+        double meter = altitude * 0.0254 * 12D;
+        long mUnit = (long)meter;
+        long mFraction = (long)((meter - mUnit) * 1000000000);
+        return mUnit << 32 | mFraction;
+    }
+
+    /***
+     * Altitude, in metres and fractional metres. The units are in the high 32-bit integer (at 0574) and the fractional part is in the low 32-bit integer (at 0570).
+     */
 	public static double toAlititude(long fsuipcAltitude) {		
 		long mUnit = fsuipcAltitude >> 32;
         long mFraction = 0x0000FFFF & fsuipcAltitude;
         double altInMeter = Double.valueOf(mUnit + "." + mFraction);
         return new BigDecimal(altInMeter / 1609.344 * 5280D).setScale(2, RoundingMode.HALF_UP).doubleValue();
 	}
-	
-	public static long toFSUIPCAlititude(double altitude) {
-		double meter = altitude * 0.0254 * 12D;
-        long mUnit = (long)meter;
-        long mFraction = (long)((meter - mUnit) * 1000000000);        
-        return mUnit << 32 | mFraction;
-	}
-	
+
 	/* */
-	
+
+    /***
+     * *360/(65536*65536) for degrees
+     * if (hdg < 0) { hdg += 360; }
+     */
 	public static double toHeading(int fsuipcHeading) {
 		double hdg = fsuipcHeading * 8.3819E-008; //8.1716E-008D;
         if (hdg < 0) { hdg += 360; }
         return (new BigDecimal(hdg)).setScale(2, RoundingMode.HALF_UP).doubleValue();
 	}
-	
+
+    /***
+     * (int)Math.round - *360/(65536*65536) for degrees
+     */
 	public static int toFSUIPCHeading(double heading) {
 		return (int)Math.round(heading / 8.3819E-008D);
 	}
 	
 	/* */
-	
-	public static double toDegree(int fsuipcDegree) {
-		double hdg = fsuipcDegree * 8.3819E-008; //8.1716E-008D;
-        return (new BigDecimal(hdg)).setScale(2, RoundingMode.HALF_UP).doubleValue();
+
+    /***
+     * *360/(65536*65536) for degrees
+     */
+	public static double toDegree(long fsuipcDegree) {
+		double degree = fsuipcDegree * 8.3819E-008; //8.1716E-008D;
+        return (new BigDecimal(degree)).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
-	
+
+    /***
+     * *360/(65536*65536) for degrees
+     */
 	public static long toFSUIPCDegree(double degree) {
 		return (long)(degree / 8.3819E-008D);
 	}
--- fsuipc/src/test/java/de/newsarea/homecockpit/fsuipc/util/FSUIPCUtilTest.java
+++ fsuipc/src/test/java/de/newsarea/homecockpit/fsuipc/util/FSUIPCUtilTest.java
@@ -30,10 +30,40 @@ public class FSUIPCUtilTest {
 
     @Test
     public void shouldConvertFSUIPCLatitudeToLatitude() {
+        assertEquals(90D, FSUIPCUtil.toLatitude(42957189152768000L));
+        assertEquals(-90D, FSUIPCUtil.toLatitude(-42957189152768000L));
         assertEquals(32.012D, FSUIPCUtil.toLatitude(15279394879537880L));
         assertEquals(38.898556, FSUIPCUtil.toLatitude(18566362531794872L));
     }
 
+    @Test
+    public void shouldConvertLongitudeToFSUIPCLongitude() {
+        assertEquals(9223372036854775807L, FSUIPCUtil.toFSUIPCLongitude(180D));
+        assertEquals(-9223372036854775808L, FSUIPCUtil.toFSUIPCLongitude(-180D));
+        assertEquals(1640325475798861568L, FSUIPCUtil.toFSUIPCLongitude(32.012D));
+        assertEquals(1993199187135719680L, FSUIPCUtil.toFSUIPCLongitude(38.898556D));
+    }
+
+    @Test
+    public void shouldConvertFSUIPCLongitudeToLongitude() {
+        assertEquals(180D, FSUIPCUtil.toLongitude(9223372036854775807L));
+        assertEquals(-180D, FSUIPCUtil.toLongitude(-9223372036854775808L));
+        assertEquals(32.012D, FSUIPCUtil.toLongitude(1640325475798861568L));
+        assertEquals(38.898556D, FSUIPCUtil.toLongitude(1993199187135719680L));
+    }
+
+    @DataProvider(name = "invalidLongitudeValues")
+    public Object[][] invalidLongitudeValues() {
+        return new Object[][] {
+                { -181 },
+                { 181 }
+        };
+    }
+
+    @Test(dataProvider = "invalidLongitudeValues", expectedExceptions = IllegalArgumentException.class)
+    public void shouldNotConvertToFSUIPCLongitude_ArgumentOutOfBound(Integer invalidLatitudeValue) throws Exception {
+        FSUIPCUtil.toFSUIPCLatitude(invalidLatitudeValue);
+    }
 
     @Test
     public void shouldConvertToAlititude() {
@@ -43,6 +73,32 @@ public class FSUIPCUtilTest {
     @Test
     public void shouldConvertToFSUIPCAlititude() {
         assertEquals(90621753215L, FSUIPCUtil.toFSUIPCAlititude(70.3D));
+    }
+
+    @Test
+    public void shouldConvertFromHeadingToFSUIPCHeading() throws Exception {
+        assertEquals(1073742230, FSUIPCUtil.toFSUIPCHeading(90));
+        assertEquals(-2147482835, FSUIPCUtil.toFSUIPCHeading(180));
+        assertEquals(-11928844, FSUIPCUtil.toFSUIPCHeading(359));
+    }
+
+    @Test
+    public void shouldConvertFromFSUIPCHeadingToHeading() throws Exception {
+        assertEquals(90D, FSUIPCUtil.toHeading(1073742230));
+        assertEquals(180D, FSUIPCUtil.toHeading(-2147482835));
+        assertEquals(359D, FSUIPCUtil.toHeading(-11928844));
+    }
+
+    @Test
+    public void shouldConvertFromDegreeToFSUIPCDegree() throws Exception {
+        assertEquals(1073742230L, FSUIPCUtil.toFSUIPCDegree(90D));
+        assertEquals(2147484460L, FSUIPCUtil.toFSUIPCDegree(180D));
+    }
+
+    @Test
+    public void shouldConvertFromFSUIPCDegreeToDegree() throws Exception {
+        assertEquals(90D, FSUIPCUtil.toDegree(1073742230L));
+        assertEquals(180D, FSUIPCUtil.toDegree(2147484460L));
     }
 
 }
